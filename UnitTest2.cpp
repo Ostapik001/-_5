3c0b439569cf58a416e374295b14677b2cec1814
Ostapik001 <115142070+Ostapik001@users.noreply.github.com>
2022-11-02 20:07:44 +0200

    Add files via upload

---
+++ UnitTest2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../АП_5.0/АП_5.0.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = sum(2, 3);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
